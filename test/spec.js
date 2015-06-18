c99782fe17827866ca4453971d9204a6a91093e7
Morris Singer <morrissinger@gmail.com>
2015-06-18 13:37:35 -0400

    Update spec.js

--- test/spec.js
+++ test/spec.js
@@ -8,7 +8,7 @@ describe('chai and plugins', function () {
     assert(true, 'true is true');
   });
 
-  describe('chai-as-promised', function () {
+  describe('chai-fuzzy', function () {
     it('should', function () {
       return Promise.resolve(true).should.eventually.equal(true);
     });
@@ -22,27 +22,5 @@ describe('chai and plugins', function () {
     });
   });
 
-  it('sinon-chai', function () {
-    var spy = sinon.spy();
-
-    spy(true);
-
-    spy.should.have.been.calledWith(true);
-    expect(spy).to.have.been.calledWith(true);
-  });
-
-  it('chai-jquery', function () {
-    var $elem = $('<div class="test"></div>');
-
-    $elem.should.have.class('test');
-    expect($elem).to.have.class('test');
-  });
-
-  it('chai-things', function () {
-    var array = [4, 11, 15];
-
-    array.should.include.one.below(10);
-    expect(array).to.include.one.below(10);
-  });
 });
 
